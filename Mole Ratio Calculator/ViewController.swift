911e35969a324be1bcef93a843a78089b6b7ad3c
Tim Snow <tim.snow@cunninglemon.com>
2015-05-25 21:54:58 +0100

    Initial Commit

---
+++ Mole Ratio Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mole Ratio Calculator
+//
+//  Created by Tim Snow on 25/05/2015.
+//  Copyright (c) 2015 Tim Snow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
