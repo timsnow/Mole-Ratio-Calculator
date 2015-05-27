9b8cfde8ecd7db8475fafaef0070d331a8cc4902
Tim Snow <tim.snow@cunninglemon.com>
2015-05-28 00:26:45 +0100

    Changes & Manual
    A finished writing the program and the manual

--- Mole Ratio Calculator/ViewController.swift
+++ Mole Ratio Calculator/ViewController.swift
@@ -32,10 +32,10 @@ class ViewController: NSViewController, NSTextFieldDelegate  {
     @IBOutlet weak var dMolarityInputField: NSTextField!
 
     // Declare all the outputs to the storyboard
-    @IBOutlet weak var aMolarityLabel: NSTabView!
-    @IBOutlet weak var bMolarityLabel: NSTabView!
-    @IBOutlet weak var cMolarityLabel: NSTabView!
-    @IBOutlet weak var dMolarityLabel: NSTabView!
+    @IBOutlet weak var aMolarityLabel: NSTextField!
+    @IBOutlet weak var bMolarityLabel: NSTextField!
+    @IBOutlet weak var cMolarityLabel: NSTextField!
+    @IBOutlet weak var dMolarityLabel: NSTextField!
     @IBOutlet weak var aMassLabel: NSTextField!
     @IBOutlet weak var bMassLabel: NSTextField!
     @IBOutlet weak var cMassLabel: NSTextField!
@@ -70,25 +70,24 @@ class ViewController: NSViewController, NSTextFieldDelegate  {
         dMWeightInputField.delegate = self
         dMWeightInputField.stringValue = "1"
         aMassInputField.delegate = self
-        aMassInputField.stringValue = "1"
+        aMassInputField.stringValue = "0"
         bMassInputField.delegate = self
-        bMassInputField.stringValue = "1"
+        bMassInputField.stringValue = "0"
         cMassInputField.delegate = self
-        cMassInputField.stringValue = "1"
+        cMassInputField.stringValue = "0"
         dMassInputField.delegate = self
-        dMassInputField.stringValue = "1"
+        dMassInputField.stringValue = "0"
         aMolarityInputField.delegate = self
-        aMolarityInputField.stringValue = "1"
+        aMolarityInputField.stringValue = "0"
         bMolarityInputField.delegate = self
-        bMolarityInputField.stringValue = "1"
+        bMolarityInputField.stringValue = "0"
         cMolarityInputField.delegate = self
-        cMolarityInputField.stringValue = "1"
+        cMolarityInputField.stringValue = "0"
         dMolarityInputField.delegate = self
-        dMolarityInputField.stringValue = "1"
+        dMolarityInputField.stringValue = "0"
 
         // Update the labels with some calculated values
-        molFromMass()
-        massFromMol()
+        // Errr...
     }
 
     override var representedObject: AnyObject? {
@@ -100,30 +99,195 @@ class ViewController: NSViewController, NSTextFieldDelegate  {
 
    override func controlTextDidChange (notification: NSNotification) {
         // Listen for NSNotifications when the input field values change and handle them
-        if  (notification.object?.description == aMassInputField.description) || (notification.object?.description == bMassInputField.description) || (notification.object?.description == cMassInputField.description) || (notification.object?.description == dMassInputField.description) {
-            molFromMass()
-        } 
-        else if (notification.object?.description == aMolarityInputField.description) || (notification.object?.description == bMolarityInputField.description) || (notification.object?.description == cMolarityInputField.description) || (notification.object?.description == dMolarityInputField.description) {
-            massFromMol()
+        if (notification.object?.description == aRatioInputField.description) || (notification.object?.description == aMWeightInputField.description) || (notification.object?.description == aMassInputField.description) || (notification.object?.description == aMolarityInputField.description) {
+            calculateFromA()
         }
-        else {
-            molFromMass()
-            massFromMol()
-       }
+        else if (notification.object?.description == bRatioInputField.description) || (notification.object?.description == bMWeightInputField.description) || (notification.object?.description == bMassInputField.description) || (notification.object?.description == bMolarityInputField.description) {
+            calculateFromB()
+        }
+        else if (notification.object?.description == cRatioInputField.description) || (notification.object?.description == cMWeightInputField.description) || (notification.object?.description == cMassInputField.description) || (notification.object?.description == cMolarityInputField.description) {
+            calculateFromC()
+        }
+        else if (notification.object?.description == dRatioInputField.description) || (notification.object?.description == dMWeightInputField.description) || (notification.object?.description == dMassInputField.description) || (notification.object?.description == dMolarityInputField.description) {
+            calculateFromD()
+        }
     }
 
-    func molFromMass() {
+    func calculateFromA() {
         // Do some maths based on the user input
         // Start by getting the values from the input fields and converting them to floats
         updateValuesFromUI()
         
+        // First for masses
+        // Work out values
+        var aInMol = self.massA / self.mwA
+        var universalMol = aInMol / self.ratioA
+        var bInMol = universalMol * ratioB
+        var bAsMass = bInMol * self.mwB
+        var cInMol = universalMol * ratioC
+        var cAsMass = cInMol * self.mwC
+        var dInMol = universalMol * ratioD
+        var dAsMass = dInMol * self.mwD
+        // Set values
+        aMolarityLabel.stringValue = String(format: "%5.3f", aInMol)
+        bMolarityLabel.stringValue = String(format: "%5.3f", bInMol)
+        cMolarityLabel.stringValue = String(format: "%5.3f", cInMol)
+        dMolarityLabel.stringValue = String(format: "%5.3f", dInMol)
+        bMassInputField.stringValue = String(format: "%5.3f", bAsMass)
+        cMassInputField.stringValue = String(format: "%5.3f", cAsMass)
+        dMassInputField.stringValue = String(format: "%5.3f", dAsMass)
+        
+        // Then for molarities
+        // Work out values
+        universalMol = self.molarityA / self.ratioA
+        var aAsMass = self.molarityA * self.mwA
+        bInMol = universalMol * ratioB
+        bAsMass = bInMol * self.mwB
+        cInMol = universalMol * ratioC
+        cAsMass = cInMol * self.mwC
+        dInMol = universalMol * ratioD
+        dAsMass = dInMol * self.mwD
+        // Set values
+        aMassLabel.stringValue = String(format: "%5.3f", aAsMass)
+        bMassLabel.stringValue = String(format: "%5.3f", bAsMass)
+        cMassLabel.stringValue = String(format: "%5.3f", cAsMass)
+        dMassLabel.stringValue = String(format: "%5.3f", dAsMass)
+        bMolarityInputField.stringValue = String(format: "%5.3f", bInMol)
+        cMolarityInputField.stringValue = String(format: "%5.3f", cInMol)
+        dMolarityInputField.stringValue = String(format: "%5.3f", dInMol)
     }
 
-    func massFromMol() {
+    func calculateFromB() {
         // Do some maths based on the user input
         // Start by getting the values from the input fields and converting them to floats
         updateValuesFromUI()
         
+        // First for masses
+        // Work out values
+        var bInMol = self.massB / self.mwB
+        var universalMol = bInMol / self.ratioB
+        var aInMol = universalMol * ratioA
+        var aAsMass = aInMol * self.mwA
+        var cInMol = universalMol * ratioC
+        var cAsMass = cInMol * self.mwC
+        var dInMol = universalMol * ratioD
+        var dAsMass = dInMol * self.mwD
+        // Set values
+        aMolarityLabel.stringValue = String(format: "%5.3f", aInMol)
+        bMolarityLabel.stringValue = String(format: "%5.3f", bInMol)
+        cMolarityLabel.stringValue = String(format: "%5.3f", cInMol)
+        dMolarityLabel.stringValue = String(format: "%5.3f", dInMol)
+        aMassInputField.stringValue = String(format: "%5.3f", aAsMass)
+        cMassInputField.stringValue = String(format: "%5.3f", cAsMass)
+        dMassInputField.stringValue = String(format: "%5.3f", dAsMass)
+        
+        // Then for molarities
+        // Work out values
+        universalMol = self.molarityB / self.ratioB
+        var bAsMass = self.molarityB * self.mwB
+        aInMol = universalMol * ratioA
+        aAsMass = aInMol * self.mwA
+        cInMol = universalMol * ratioC
+        cAsMass = cInMol * self.mwC
+        dInMol = universalMol * ratioD
+        dAsMass = dInMol * self.mwD
+        // Set values
+        aMassLabel.stringValue = String(format: "%5.3f", aAsMass)
+        bMassLabel.stringValue = String(format: "%5.3f", bAsMass)
+        cMassLabel.stringValue = String(format: "%5.3f", cAsMass)
+        dMassLabel.stringValue = String(format: "%5.3f", dAsMass)
+        aMolarityInputField.stringValue = String(format: "%5.3f", aInMol)
+        cMolarityInputField.stringValue = String(format: "%5.3f", cInMol)
+        dMolarityInputField.stringValue = String(format: "%5.3f", dInMol)
+        
+    }
+
+    func calculateFromC() {
+        // Do some maths based on the user input
+        // Start by getting the values from the input fields and converting them to floats
+        updateValuesFromUI()
+        
+        // First for masses
+        // Work out values
+        var cInMol = self.massC / self.mwC
+        var universalMol = cInMol / self.ratioC
+        var bInMol = universalMol * ratioB
+        var bAsMass = bInMol * self.mwB
+        var aInMol = universalMol * ratioA
+        var aAsMass = aInMol * self.mwA
+        var dInMol = universalMol * ratioD
+        var dAsMass = dInMol * self.mwD
+        // Set values
+        aMolarityLabel.stringValue = String(format: "%5.3f", aInMol)
+        bMolarityLabel.stringValue = String(format: "%5.3f", bInMol)
+        cMolarityLabel.stringValue = String(format: "%5.3f", cInMol)
+        dMolarityLabel.stringValue = String(format: "%5.3f", dInMol)
+        bMassInputField.stringValue = String(format: "%5.3f", bAsMass)
+        aMassInputField.stringValue = String(format: "%5.3f", aAsMass)
+        dMassInputField.stringValue = String(format: "%5.3f", dAsMass)
+        
+        // Then for molarities
+        // Work out values
+        universalMol = self.molarityC / self.ratioC
+        var cAsMass = self.molarityC * self.mwC
+        bInMol = universalMol * ratioB
+        bAsMass = bInMol * self.mwB
+        aInMol = universalMol * ratioA
+        aAsMass = aInMol * self.mwA
+        dInMol = universalMol * ratioD
+        dAsMass = dInMol * self.mwD
+        // Set values
+        aMassLabel.stringValue = String(format: "%5.3f", aAsMass)
+        bMassLabel.stringValue = String(format: "%5.3f", bAsMass)
+        cMassLabel.stringValue = String(format: "%5.3f", cAsMass)
+        dMassLabel.stringValue = String(format: "%5.3f", dAsMass)
+        bMolarityInputField.stringValue = String(format: "%5.3f", bInMol)
+        aMolarityInputField.stringValue = String(format: "%5.3f", aInMol)
+        dMolarityInputField.stringValue = String(format: "%5.3f", dInMol)
+    }
+
+    func calculateFromD() {
+        // Do some maths based on the user input
+        // Start by getting the values from the input fields and converting them to floats
+        updateValuesFromUI()
+        
+        // First for masses
+        // Work out values
+        var dInMol = self.massD / self.mwD
+        var universalMol = dInMol / self.ratioD
+        var bInMol = universalMol * ratioB
+        var bAsMass = bInMol * self.mwB
+        var cInMol = universalMol * ratioC
+        var cAsMass = cInMol * self.mwC
+        var aInMol = universalMol * ratioA
+        var aAsMass = aInMol * self.mwA
+        // Set values
+        aMolarityLabel.stringValue = String(format: "%5.3f", aInMol)
+        bMolarityLabel.stringValue = String(format: "%5.3f", bInMol)
+        cMolarityLabel.stringValue = String(format: "%5.3f", cInMol)
+        dMolarityLabel.stringValue = String(format: "%5.3f", dInMol)
+        bMassInputField.stringValue = String(format: "%5.3f", bAsMass)
+        cMassInputField.stringValue = String(format: "%5.3f", cAsMass)
+        aMassInputField.stringValue = String(format: "%5.3f", aAsMass)
+        
+        // Then for molarities
+        // Work out values
+        universalMol = self.molarityD / self.ratioD
+        var dAsMass = self.molarityD * self.mwD
+        bInMol = universalMol * ratioB
+        bAsMass = bInMol * self.mwB
+        cInMol = universalMol * ratioC
+        cAsMass = cInMol * self.mwC
+        aInMol = universalMol * ratioA
+        aAsMass = aInMol * self.mwA
+        // Set values
+        aMassLabel.stringValue = String(format: "%5.3f", aAsMass)
+        bMassLabel.stringValue = String(format: "%5.3f", bAsMass)
+        cMassLabel.stringValue = String(format: "%5.3f", cAsMass)
+        dMassLabel.stringValue = String(format: "%5.3f", dAsMass)
+        bMolarityInputField.stringValue = String(format: "%5.3f", bInMol)
+        cMolarityInputField.stringValue = String(format: "%5.3f", cInMol)
+        aMolarityInputField.stringValue = String(format: "%5.3f", aInMol)
     }
 
     
